a5d27495335d2f5ba52fa9b64109f218a9b5fefc
Elina <khamitovaElina09@gmail.com>
2023-03-02 14:22:59 +0300

    Add project files.

---
+++ BiblApp/KabinetPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BiblApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class KabinetPage : ContentPage
+    {
+        Label stackLabel;
+        public KabinetPage()
+        {
+            Title = "Page 2";
+            Button booksBtn = new Button { Text = "К списку книг" };
+            booksBtn.Clicked += GoToForward;
+
+            Button regBtn = new Button { Text = "Назад" };
+            regBtn.Clicked += GoToBack;
+
+            stackLabel = new Label();
+            Content = new StackLayout { Children = { booksBtn, regBtn, stackLabel } };
+        }
+        protected internal void DisplayStack()
+        {
+            NavigationPage navPage = (NavigationPage)App.Current.MainPage;
+            stackLabel.Text = "";
+            foreach (Page p in navPage.Navigation.NavigationStack)
+            {
+                stackLabel.Text += p.Title + "\n";
+            }
+        }
+        // Переход вперед на Page3
+        private async void GoToForward(object sender, EventArgs e)
+        {
+            BooksPage page = new BooksPage();
+            await Navigation.PushAsync(page);
+            page.DisplayStack();
+        }
+        // Переход обратно на MainPage
+        private async void GoToBack(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+
+            NavigationPage navPage = (NavigationPage)App.Current.MainPage;
+            ((RegPage)navPage.CurrentPage).DisplayStack();
+        }
+
+    }
+}
---
+++ BiblApp/RegPage.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace BiblApp
+{
+    public partial class RegPage : ContentPage
+    {
+        bool loaded = false;
+        Label stackLabel;
+        public RegPage()
+        {
+            InitializeComponent();
+            stackLabel = new Label();
+            Content = new StackLayout { Children = { stackLabel } };
+
+
+        }
+        protected internal void DisplayStack()
+        {
+            NavigationPage navPage = (NavigationPage)App.Current.MainPage;
+            stackLabel.Text = "";
+            foreach (Page p in navPage.Navigation.NavigationStack)
+            {
+                stackLabel.Text += p.Title + "\n";
+            }
+        }
+        void rolePicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private async void OnButtonClicked(object sender, System.EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (login.Text == null)
+            {
+                DisplayAlert("Неуспешно", "Введите логин", "Ок");
+            }
+            else if (email.Text == null)
+            {
+                DisplayAlert("Неуспешно", "Введите почту", "Ок");
+            }
+            else if (password.Text == null || confirmPassword == null)
+            {
+                DisplayAlert("Неуспешно", "Введите пароль", "Ок");
+            }
+            else if (password.Text != confirmPassword.Text)
+            {
+                DisplayAlert("Неуспешно", "Неправильно повторили пароль", "Ок");
+            }
+            else if (phone.Text == null)
+            {
+                DisplayAlert("Неуспешно", "Введите телефон", "Ок");
+            }
+            else if (role.SelectedItem == null)
+            {
+                DisplayAlert("Неуспешно", "Укажите роль", "Ок");
+            }
+            else
+            {
+                DisplayAlert("Успешно", "Вы зарегистрировались", "Ок");
+               
+                KabinetPage page = new KabinetPage();
+                await Navigation.PushAsync(page);
+                page.DisplayStack();
+
+            }
+        }
+        
+
+        private void Theme_OnButtonClicked(object sender, System.EventArgs e)
+        {
+            //int k = 0;
+            //k++;
+            if (BackgroundColor == Color.White)
+            {
+                BackgroundColor = Color.Black;
+                regLabel.TextColor = Color.White;
+                header.TextColor = Color.White;
+                email.TextColor = Color.White;
+                password.TextColor = Color.White;
+                confirmPassword.TextColor = Color.White;
+                phone.TextColor = Color.White;
+                login.TextColor = Color.White;
+                dateLable.TextColor = Color.White;
+                role.TextColor = Color.White;
+                datePic.TextColor = Color.White;
+            }
+            else 
+            {
+                BackgroundColor = Color.White;
+                regLabel.TextColor = Color.Black;
+                header.TextColor = Color.Black;
+                email.TextColor = Color.Black;
+                password.TextColor = Color.Black;
+                confirmPassword.TextColor = Color.Black;
+                phone.TextColor = Color.Black;
+                login.TextColor = Color.Black;
+                dateLable.TextColor = Color.Black;
+                role.TextColor = Color.Black;
+                datePic.TextColor = Color.Black;
+            }
+
+
+
+        }
+        private void Exit_OnButtonClicked(object sender, System.EventArgs e)
+        {
+            Thread.CurrentThread.Abort();
+        }
+
+    }
+}
